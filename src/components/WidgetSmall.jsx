d0a6b2d39ca1d2e848e6d5d3ae6a31fd0cc6e3a9
muhammadderic <muhammad.donny.e@gmail.com>
2023-05-23 21:13:52 +0700

    WidgetSmall update

---
+++ src/components/WidgetSmall.jsx
@@ -0,0 +1,84 @@
+export default function WidgetSmall() {
+  return (
+    <div className="widgetSmall">
+      <span className="widgetSmallTitle">New Join Members</span>
+      <ul className="widgetSmallList">
+        <li className="widgetSmallItem">
+          <img src="http://placekitten.com/50/50" alt="" className="widgetSmallImg" />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUserName">User 1</span>
+            <span className="widgetSmallUserJob">Tech Lead</span>
+          </div>
+          <div className="widgetSmallButton">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+              <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
+            </svg>
+            Display
+          </div>
+        </li>
+        <li className="widgetSmallItem">
+          <img src="http://placekitten.com/51/51" alt="" className="widgetSmallImg" />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUserName">User 2</span>
+            <span className="widgetSmallUserJob">Software Engineer</span>
+          </div>
+          <div className="widgetSmallButton">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+              <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
+            </svg>
+            Display
+          </div>
+        </li>
+        <li className="widgetSmallItem">
+          <img src="http://placekitten.com/52/52" alt="" className="widgetSmallImg" />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUserName">User 3</span>
+            <span className="widgetSmallUserJob">Front End Developer</span>
+          </div>
+          <div className="widgetSmallButton">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+              <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
+            </svg>
+            Display
+          </div>
+        </li>
+        <li className="widgetSmallItem">
+          <img src="http://placekitten.com/53/53" alt="" className="widgetSmallImg" />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUserName">User 4</span>
+            <span className="widgetSmallUserJob">Back End Developer</span>
+          </div>
+          <div className="widgetSmallButton">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+              <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
+            </svg>
+            Display
+          </div>
+        </li>
+        <li className="widgetSmallItem">
+          <img src="http://placekitten.com/54/54" alt="" className="widgetSmallImg" />
+          <div className="widgetSmallUser">
+            <span className="widgetSmallUserName">User 5</span>
+            <span className="widgetSmallUserJob">Database Admin</span>
+          </div>
+          <div className="widgetSmallButton">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+              <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6"></path>
+            </svg>
+            Display
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
